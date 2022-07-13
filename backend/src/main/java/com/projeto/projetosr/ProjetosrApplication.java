13c16185c5306d285fab080f2f0fd819ac6ad009
luclucas <luclucas.08@gmail.com>
2022-07-13 16:55:37 -0300

    primeiro commit

---
+++ backend/src/main/java/com/projeto/projetosr/ProjetosrApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projetosr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosrApplication.class, args);
+	}
+
+}
